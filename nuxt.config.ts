59e99efe7606afd23fcb9cfb5141fc270f9283bb
XavtheBirdGod <xanthe.vannestedeloose@gmail.com>
2026-01-07 10:46:06 +0100

    supabase connection werkt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
     modules: [
         '@nuxtjs/tailwindcss'
-    ]
+    ],
+    runtimeConfig: {
+        public: {
+            supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+            supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
+        }
+    }
 })
