636545f651e8c27cf79ec99ba4716b2d2f73645b
XavtheBirdGod <xanthe.vannestedeloose@gmail.com>
2026-01-13 14:18:21 +0100

    resourcecard + reservation counter

--- components/ResourceCard.vue
+++ components/ResourceCard.vue
@@ -8,18 +8,28 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-[#24262d] p-5 rounded-xl border border-gray-700 hover:border-blue-500 cursor-pointer transition flex justify-between items-center group">
-    <div>
+  <div class="bg-[#24262d] rounded-xl border border-gray-700 hover:border-blue-500 cursor-pointer transition flex justify-between items-center group overflow-hidden">
+
+    <div class="flex-shrink-0 w-28 h-24">
+      <img
+          :src="resource.image_url"
+          :alt="resource.title"
+          class="w-full h-full object-cover"
+      />
+    </div>
+
+    <div class="flex-1 px-5 py-2">
       <div class="flex items-center gap-3">
         <h3 class="text-lg font-bold text-white">{{ resource.title }}</h3>
         <span class="px-2 py-0.5 text-xs bg-blue-900/40 text-blue-300 rounded">{{ resource.type }}</span>
       </div>
       <p class="text-sm text-gray-400 mt-1">{{ resource.description }}</p>
     </div>
-    <div class="text-right">
-      <span class="text-sm font-semibold text-blue-300 bg-[#1a1c23] px-3 py-1 rounded-full border border-blue-900/40">
 
-      </span>
+    <div class="pr-5 text-right">
+      <span class="text-sm font-semibold text-blue-300 bg-[#1a1c23] px-3 py-1 rounded-full border border-blue-900/40">
+        {{ resource.reservations?.length || 0 }} Reservations
+        </span>
     </div>
   </div>
 </template>
--- stores/resources.js
+++ stores/resources.js
@@ -12,24 +12,26 @@ export const useResourceStore = defineStore('resources', {
         async fetchResources() {
             this.loading = true
             this.error = null
-
             try {
                 const supabase = useSupabase()
-
                 const { data, error } = await supabase
                     .from('resources')
-                    .select('*')
-                    // .order('created_at', { ascending: false })
+                    .select(`
+                        *,
+                        reservations (
+                            id
+                        )
+                    `)
+                    .order('title', { ascending: true })
 
                 if (error) throw error
-
                 this.resources = data
             } catch (err) {
-                console.error("Resource Store Error:", err);
+                console.error("Resource Store Error:", err)
                 this.error = err.message
             } finally {
                 this.loading = false
             }
-        }
+        },
     }
 })
